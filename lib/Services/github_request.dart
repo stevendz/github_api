34e9437e821c334b51da7903a049b5e1011398cd
Steven Dz <Steven.Dzionara@gmail.com>
2020-08-18 11:22:47 +0200

    Update github_request.dart

--- lib/Services/github_request.dart
+++ lib/Services/github_request.dart
@@ -3,8 +3,8 @@ import 'package:http/http.dart' as http;
 class Github {
   final String userName;
   final String url = 'https://api.github.com/';
-  static String clientId = '69245e68bba3307c2468';
-  static String clientSecret = 'c665ae879cde135d5e1ec28668e4cbdef7d3ad5d';
+  static String clientId = 'YOUR_CLIENT_ID';
+  static String clientSecret = 'YOUR_CLIENT_SECRET';
 
   final String query = "?client_id=$clientId&client_secret=$clientSecret";
 
